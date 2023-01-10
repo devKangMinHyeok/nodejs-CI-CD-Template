d7093b520d7150181fcc096139d5cc33e8aa963c
Kang <rkdalsgur032@unist.ac.kr>
2023-01-10 17:48:55 +0900

    test code

---
+++ test/integration/index.test.js
@@ -0,0 +1,5 @@
+import {main} from '../../src/index';
+
+test('main 1 , 2 to equal 2', () => {
+  expect(main(1, 2)).toBe(2);
+});
---
+++ test/unit/sub.test.js
@@ -0,0 +1,5 @@
+import {sub} from '../../src/functions/add';
+
+test('sub 2 - 1 to equal 1', () => {
+  expect(sub(2, 1)).toBe(1);
+});
